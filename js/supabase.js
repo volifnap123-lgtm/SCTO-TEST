26e15989c22afec8d9420cf972a4702934537958
volifnap123-lgtm <volifnap123@gmail.com>
2026-03-27 22:43:09 +0800

    Обновление Supabase ключа

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 // Supabase Client Initialization
 const SUPABASE_URL = 'https://noskliwvsiejokzmczfp.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_VO6LwV9CJe-45tDjFgXGug_TzYoQ-6v';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5vc2tsaXd2c2llam9rem1jemZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyMzU5MTgsImV4cCI6MjA4ODgxMTkxOH0.2NplRLLx1Annta9DL8Wus-OoObQwUbYR4X_vHouDEbE';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
